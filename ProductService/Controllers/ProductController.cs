c11148b3cdde9339844334d27fd9f4f5c277af69
[Megha Thakur] <[meghathakur292@gmail.com]>
2026-03-18 23:22:11 +0530

    ProductService

---
+++ ProductService/Controllers/ProductController.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductService.Data;
+using ProductService.DTOs;
+using ProductService.Models;
+
+namespace ProductService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private readonly ProductDbContext _context;
+
+        public ProductController(ProductDbContext context)
+        {
+            _context = context;
+        }
+
+        // ➕ ADD PRODUCT
+        [HttpPost]
+        public async Task<IActionResult> AddProduct(CreateProductDto dto)
+        {
+            var product = new Product
+            {
+                Name = dto.Name,
+                Price = dto.Price,
+                Stock = dto.Stock
+            };
+
+            _context.Products.Add(product);
+            await _context.SaveChangesAsync();
+
+            return Ok(product);
+        }
+
+        // 📄 GET ALL PRODUCTS
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var products = _context.Products.ToList();
+            return Ok(products);
+        }
+
+        // 🔍 GET PRODUCT BY ID
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+                return NotFound();
+
+            return Ok(product);
+        }
+
+        // ✏️ UPDATE PRODUCT
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, UpdateProductDto dto)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+                return NotFound();
+
+            product.Name = dto.Name;
+            product.Price = dto.Price;
+            product.Stock = dto.Stock;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(product);
+        }
+
+        // ❌ DELETE PRODUCT
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var product = await _context.Products.FindAsync(id);
+
+            if (product == null)
+                return NotFound();
+
+            _context.Products.Remove(product);
+            await _context.SaveChangesAsync();
+
+            return Ok("Deleted Successfully");
+        }
+    }
+}
---
+++ ProductService/DTOs/UpdateProductDto.cs
@@ -0,0 +1,9 @@
+﻿namespace ProductService.DTOs
+{
+    public class UpdateProductDto
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+    }
+}
